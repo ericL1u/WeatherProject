cdebd97a854ffc56a69cf98e2ecc548d6b0f4bab
ericL1u <rxyhliu@gmail.com>
2021-12-13 00:27:05 -0600

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+
+
+app.get("/" , function(req,res){
+  res.send("Server is running")
+})
+
+app.listen(3000 , function(){
+  console.log("Sever is running on port 3000")
+})
