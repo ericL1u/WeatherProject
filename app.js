92c7b8230d0f146c775a7b4fe4aeda692edbe7d5
ericL1u <rxyhliu@gmail.com>
2021-12-13 16:41:33 -0600

    response

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
 
 
 app.get("/" , function(req,res){
-  res.send("Server is running")
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Hangzhou&appid=2dff358d9727cd778a014f944690834e"
+  https.get(url,function(response){
+    console.log(response.statusCode);
+
+    response.on("data",function(data){
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      res.send("+he temp in hangzhou" + temp + "Celcius")
+      // return
+    })
+
+  })
+
+  // res.send("Server is running")
 })
 
 app.listen(3000 , function(){
